eebb407b66db5cb5179f92cfe3172d0e4c6dfb8c
StiphyJay <601772137@qq.com>
2026-03-11 15:18:28 +0000

    Deploying to gh-pages from @ StiphyJay/StiphyJay.github.io@e99a0ccbfe46067b2f517456a8ad44c862a0c967 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "StiphyJay/SifanZhou.github.io",
+    "data-repo": "StiphyJay/StiphyJay.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
